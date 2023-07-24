6f2c4e423d4910410407ccfa1d86de4c1aad8d38
Sagar Ghimire <87696933+sagargg@users.noreply.github.com>
2023-07-24 07:56:35 +0000

    [fix][xs] Enabled chart map views

--- ckanext/dataexplorer/public/recline_view.js
+++ ckanext/dataexplorer/public/recline_view.js
@@ -207,9 +207,7 @@ this.ckan.module('recline_view', function (jQuery, _) {
           view: new recline.View.SlickGrid({
             model: dataset
           })
-        }
-        /** NOT IN USE
-        ,{
+        },{
           id: 'graph',
           label: _('Graph').fetch(),
           view: new recline.View.Graph({
@@ -221,7 +219,6 @@ this.ckan.module('recline_view', function (jQuery, _) {
           label: _('Map').fetch(),
           view: new recline.View.Map(this._reclineMapViewOptions(dataset, map_config))
         }
-        **/
       ];
 
       views = views.filter(function(view) {
